0f00fd6d6173b51ae7a9055afcb2f3392201b89f
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-12-21 14:31:26 +0100

    update

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+
+// alert("fine")
+
+function checkpassword(){
+	let pass1 = document.getElementById("pass1").value;
+	let pass2 = document.getElementById("pass2").value;
+	if(pass1 == pass2){
+		//do nothing
+	}
+	else{
+		alert("passwords dont match")
+	}
+}
+
